a1f41a3705a35cbbee546c68a9d5b33eab8c499b
zfletch <zfletch2@gmail.com>
2019-02-14 17:07:00 -0500

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
 const About = () => (
-  <div>
-    About
+  <div className="container pt-4">
+    <div className="row">
+      <div className="col-md-8 offset-md-2 text-center">
+        <h2>About <em>A Glossary of Greek Birds</em></h2>
+      </div>
+    </div>
+
+    <div className="row pt-3">
+      <div className="col-12 col-md-8 offset-md-2 text-justify">
+        <p>
+          Fusce velit tellus, volutpat vel nisl sed, facilisis gravida erat. Sed efficitur magna vel velit mollis mattis. Nam pellentesque tempor tempor. Maecenas vitae auctor massa. Etiam aliquet leo enim, fermentum vehicula massa scelerisque eget. Fusce porttitor sagittis libero a rhoncus. Cras tincidunt cursus velit at molestie. Sed vitae nulla ultrices, commodo sem tempus, congue nibh. Vivamus ut feugiat felis, vitae posuere libero. Mauris facilisis ex sit amet aliquet sodales. Fusce posuere, eros nec accumsan egestas, velit velit rutrum ante, quis aliquet ligula turpis vitae dolor.
+        </p>
+        <p>
+          Mauris placerat rutrum eros, semper vehicula metus vehicula non. Aliquam feugiat egestas sapien placerat congue. Cras sit amet felis in nibh volutpat efficitur. Donec sed nisi lacus. Aenean eget rutrum elit. Mauris lacus eros, imperdiet ac massa ac, fermentum finibus ligula. Integer in dolor eu eros euismod faucibus quis in velit. Sed venenatis, nisi vitae viverra pellentesque, sapien mi vehicula sem, eget vulputate magna purus vitae elit. Pellentesque sed ultricies velit. Curabitur fringilla euismod pulvinar. Praesent ut dui ultrices velit gravida dictum. Fusce augue eros, malesuada a molestie sit amet, maximus sed dolor. Aenean pellentesque, tellus nec tincidunt sollicitudin, massa turpis malesuada odio, ut malesuada tortor ante et tortor. Proin suscipit sapien sit amet augue aliquet, eu congue turpis iaculis. Nulla mi nisl, viverra nec magna at, placerat suscipit magna.
+        </p>
+        <p>
+          Cras mollis diam purus, sed suscipit lacus aliquet quis. Aenean pellentesque nisl sed turpis aliquet elementum. Integer in felis leo. Pellentesque arcu libero, facilisis vitae enim at, blandit pharetra nibh. Mauris molestie ut diam quis dictum. Nullam blandit euismod accumsan. Donec ipsum tellus, pharetra id metus ac, bibendum consectetur enim. Morbi commodo nec sapien sit amet tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla sed nulla euismod, porta lacus non, dignissim velit. Etiam accumsan justo quis neque dictum imperdiet. Mauris posuere fringilla diam suscipit hendrerit.
+        </p>
+      </div>
+    </div>
   </div>
 );
 
