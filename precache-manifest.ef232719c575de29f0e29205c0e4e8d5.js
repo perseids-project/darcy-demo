2e8604d1fb65175072e090bd5f8187793cb4fa41
zfletch <zfletch2@gmail.com>
2019-02-19 11:30:08 -0500

    Updates

---
+++ precache-manifest.ef232719c575de29f0e29205c0e4e8d5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "230a4f42fe6d977af0c6",
+    "url": "/darcy-demo/static/css/main.37b3c25b.chunk.css"
+  },
+  {
+    "revision": "230a4f42fe6d977af0c6",
+    "url": "/darcy-demo/static/js/main.230a4f42.chunk.js"
+  },
+  {
+    "revision": "326ae78e0d36dc272174",
+    "url": "/darcy-demo/static/js/runtime~main.326ae78e.js"
+  },
+  {
+    "revision": "3627e2c4e40a1de77fde",
+    "url": "/darcy-demo/static/css/2.1ca3a0f4.chunk.css"
+  },
+  {
+    "revision": "3627e2c4e40a1de77fde",
+    "url": "/darcy-demo/static/js/2.3627e2c4.chunk.js"
+  },
+  {
+    "revision": "a33ed7c4de68e09ab2661cccc64f16c8",
+    "url": "/darcy-demo/static/media/aithyia.a33ed7c4.png"
+  },
+  {
+    "revision": "7d8e9ca56cc83ee25d264edbe224472a",
+    "url": "/darcy-demo/static/media/glaux.7d8e9ca5.png"
+  },
+  {
+    "revision": "0de84e763750500735cc644449788809",
+    "url": "/darcy-demo/static/media/keleos.0de84e76.png"
+  },
+  {
+    "revision": "bb83941b3511d850ca9bea826c35f65a",
+    "url": "/darcy-demo/static/media/birds.bb83941b.jpg"
+  },
+  {
+    "revision": "399b183da9f1742ada882a566bb1f5af",
+    "url": "/darcy-demo/static/media/aithyia.399b183d.png"
+  },
+  {
+    "revision": "046ac0c7bbc28389a266f1f0bae21c38",
+    "url": "/darcy-demo/static/media/glaux.046ac0c7.png"
+  },
+  {
+    "revision": "8db6768445d6724712a29e79bac8aa6d",
+    "url": "/darcy-demo/static/media/keleos.8db67684.png"
+  },
+  {
+    "revision": "48438f59f1a9aab6b805d57303e7a385",
+    "url": "/darcy-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/darcy-demo/precache-manifest.6b38319a1184e02bc264a1a92220ad18.js"
+  "/darcy-demo/precache-manifest.ef232719c575de29f0e29205c0e4e8d5.js"
 );
 
 workbox.clientsClaim();
