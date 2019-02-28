4771fc1a16d8810d475802f21b97d783fc21d15d
zfletch <zfletch2@gmail.com>
2019-02-28 13:37:27 -0500

    styling for bird pages

--- src/components/Bird/Aithyia.js
+++ src/components/Bird/Aithyia.js
@@ -12,59 +12,25 @@ const Aithyia = () => (
       </div>
     </div>
 
-    <div className="d-none d-lg-block">
-      <div className="row pt-3">
-        <div className="col-12 col-md-4 offset-md-2 text-justify">
-          <p>
-            Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
-          </p>
-          <p>
-            Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <div className={`col-12 col-md-4 ${styles.imageContainer}`}>
-          <p>
-            <img src={aithyia} alt="aithyia" className={styles.image} />
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-8 offset-md-2 text-justify">
-          <p>
-            Quisque aliquet interdum gravida. Sed sagittis diam in diam egestas iaculis. Nunc sit amet turpis ac eros vehicula mollis. Curabitur ex turpis, bibendum sed tortor ac, blandit eleifend enim. Maecenas sodales leo in sollicitudin tempus. Vestibulum urna odio, sagittis eu molestie at, dignissim vitae urna. Mauris aliquam dictum mi et viverra. Cras ullamcorper tempus elit, in molestie quam aliquam a. Ut feugiat vel nisl non eleifend.
-          </p>
-          <p>
-            Etiam volutpat dignissim aliquet. Nullam sed massa ac urna semper gravida vitae sit amet elit. Maecenas pellentesque sem non velit vehicula, ac dapibus magna condimentum. Nullam euismod massa ligula, non eleifend mauris euismod et. Ut gravida egestas maximus. Ut sed orci vitae felis consectetur lobortis sed vitae magna. Nullam nec nisl fringilla, suscipit odio et, consectetur ex. Suspendisse nec malesuada orci.
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div className="d-lg-none">
-      <div className="row pt-3">
-        <div className="col-12 col-md-4 offset-md-2 text-justify">
-          <p>
-            Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
-          </p>
-        </div>
-        <div className={`col-12 col-md-4 ${styles.imageContainer}`}>
-          <p>
-            <img src={aithyia} alt="aithyia" className={styles.image} />
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-8 offset-md-2 text-justify">
-          <p>
-            Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Quisque aliquet interdum gravida. Sed sagittis diam in diam egestas iaculis. Nunc sit amet turpis ac eros vehicula mollis. Curabitur ex turpis, bibendum sed tortor ac, blandit eleifend enim. Maecenas sodales leo in sollicitudin tempus. Vestibulum urna odio, sagittis eu molestie at, dignissim vitae urna. Mauris aliquam dictum mi et viverra. Cras ullamcorper tempus elit, in molestie quam aliquam a. Ut feugiat vel nisl non eleifend.
-          </p>
-          <p>
-            Etiam volutpat dignissim aliquet. Nullam sed massa ac urna semper gravida vitae sit amet elit. Maecenas pellentesque sem non velit vehicula, ac dapibus magna condimentum. Nullam euismod massa ligula, non eleifend mauris euismod et. Ut gravida egestas maximus. Ut sed orci vitae felis consectetur lobortis sed vitae magna. Nullam nec nisl fringilla, suscipit odio et, consectetur ex. Suspendisse nec malesuada orci.
-          </p>
-        </div>
+    <div className="row pt-3">
+      <div className="col-12 col-md-8 offset-md-2 text-justify">
+        <img
+          src={aithyia}
+          alt="aithyia"
+          className={`ml-3 mb-1 mt-1 ${styles.image}`}
+        />
+        <p>
+          Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+        </p>
+        <p>
+          Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+          Quisque aliquet interdum gravida. Sed sagittis diam in diam egestas iaculis. Nunc sit amet turpis ac eros vehicula mollis. Curabitur ex turpis, bibendum sed tortor ac, blandit eleifend enim. Maecenas sodales leo in sollicitudin tempus. Vestibulum urna odio, sagittis eu molestie at, dignissim vitae urna. Mauris aliquam dictum mi et viverra. Cras ullamcorper tempus elit, in molestie quam aliquam a. Ut feugiat vel nisl non eleifend.
+        </p>
+        <p>
+          Etiam volutpat dignissim aliquet. Nullam sed massa ac urna semper gravida vitae sit amet elit. Maecenas pellentesque sem non velit vehicula, ac dapibus magna condimentum. Nullam euismod massa ligula, non eleifend mauris euismod et. Ut gravida egestas maximus. Ut sed orci vitae felis consectetur lobortis sed vitae magna. Nullam nec nisl fringilla, suscipit odio et, consectetur ex. Suspendisse nec malesuada orci.
+        </p>
       </div>
     </div>
   </div>
--- src/components/Bird/Keleos.js
+++ src/components/Bird/Keleos.js
@@ -12,53 +12,22 @@ const Keleos = () => (
       </div>
     </div>
 
-    <div className="d-none d-lg-block">
-      <div className="row pt-3">
-        <div className="col-12 col-md-4 offset-md-2 text-justify">
-          <p>
-            Quisque sed purus efficitur, semper libero sit amet, vulputate turpis. Nulla porttitor auctor blandit. Aenean eleifend egestas dapibus.
-          </p>
-          <p>
-            Fusce in turpis leo. Proin euismod interdum mauris eget dapibus. Cras semper mi urna, id hendrerit arcu ultrices eu. Ut nibh quam, aliquam ut vulputate vel, fringilla vitae urna. Nunc non finibus nulla. Vestibulum a felis sed orci vestibulum finibus in nec sapien. Sed at tortor nec odio interdum viverra non eu turpis.
-          </p>
-        </div>
-        <div className={`col-12 col-md-4 ${styles.imageContainer}`}>
-          <p>
-            <img src={keleos} alt="keleos" className={styles.image} />
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-8 offset-md-2 text-justify">
-          <p>
-            Sed semper augue in mi rhoncus elementum. Ut at fermentum arcu, nec vestibulum ante. Cras sagittis pretium neque, eu aliquet magna lobortis sit amet. Aliquam bibendum diam at tempus molestie. Proin enim erat, commodo sit amet molestie vel, eleifend eget quam. Sed nec accumsan eros, pretium egestas velit. Cras tempus sodales scelerisque. Phasellus eu cursus massa, et eleifend massa. Donec et ligula eget augue congue imperdiet. Suspendisse venenatis dui id nisl porttitor, in finibus augue placerat. Sed ligula mauris, porttitor sit amet turpis sed, luctus congue eros. Donec suscipit odio neque, id mollis lectus venenatis eu. Ut a convallis erat, at consequat odio.
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div className="d-lg-none">
-      <div className="row pt-3">
-        <div className="col-12 col-md-4 offset-md-2 text-justify">
-          <p>
-            Quisque sed purus efficitur, semper libero sit amet, vulputate turpis. Nulla porttitor auctor blandit. Aenean eleifend egestas dapibus.
-          </p>
-        </div>
-        <div className={`col-12 col-md-4 ${styles.imageContainer}`}>
-          <p>
-            <img src={keleos} alt="keleos" className={styles.image} />
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-8 offset-md-2 text-justify">
-          <p>
-            Fusce in turpis leo. Proin euismod interdum mauris eget dapibus. Cras semper mi urna, id hendrerit arcu ultrices eu. Ut nibh quam, aliquam ut vulputate vel, fringilla vitae urna. Nunc non finibus nulla. Vestibulum a felis sed orci vestibulum finibus in nec sapien. Sed at tortor nec odio interdum viverra non eu turpis.
-          </p>
-          <p>
-            Sed semper augue in mi rhoncus elementum. Ut at fermentum arcu, nec vestibulum ante. Cras sagittis pretium neque, eu aliquet magna lobortis sit amet. Aliquam bibendum diam at tempus molestie. Proin enim erat, commodo sit amet molestie vel, eleifend eget quam. Sed nec accumsan eros, pretium egestas velit. Cras tempus sodales scelerisque. Phasellus eu cursus massa, et eleifend massa. Donec et ligula eget augue congue imperdiet. Suspendisse venenatis dui id nisl porttitor, in finibus augue placerat. Sed ligula mauris, porttitor sit amet turpis sed, luctus congue eros. Donec suscipit odio neque, id mollis lectus venenatis eu. Ut a convallis erat, at consequat odio.
-          </p>
-        </div>
+    <div className="row pt-3">
+      <div className="col-12 col-md-8 offset-md-2 text-justify">
+        <img
+          src={keleos}
+          alt="keleos"
+          className={`ml-3 mb-1 mt-1 ${styles.image}`}
+        />
+        <p>
+          Quisque sed purus efficitur, semper libero sit amet, vulputate turpis. Nulla porttitor auctor blandit. Aenean eleifend egestas dapibus.
+        </p>
+        <p>
+          Fusce in turpis leo. Proin euismod interdum mauris eget dapibus. Cras semper mi urna, id hendrerit arcu ultrices eu. Ut nibh quam, aliquam ut vulputate vel, fringilla vitae urna. Nunc non finibus nulla. Vestibulum a felis sed orci vestibulum finibus in nec sapien. Sed at tortor nec odio interdum viverra non eu turpis.
+        </p>
+        <p>
+          Sed semper augue in mi rhoncus elementum. Ut at fermentum arcu, nec vestibulum ante. Cras sagittis pretium neque, eu aliquet magna lobortis sit amet. Aliquam bibendum diam at tempus molestie. Proin enim erat, commodo sit amet molestie vel, eleifend eget quam. Sed nec accumsan eros, pretium egestas velit. Cras tempus sodales scelerisque. Phasellus eu cursus massa, et eleifend massa. Donec et ligula eget augue congue imperdiet. Suspendisse venenatis dui id nisl porttitor, in finibus augue placerat. Sed ligula mauris, porttitor sit amet turpis sed, luctus congue eros. Donec suscipit odio neque, id mollis lectus venenatis eu. Ut a convallis erat, at consequat odio.
+        </p>
       </div>
     </div>
   </div>
