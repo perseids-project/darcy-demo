29ca5ba8e13ff2a965f95358d75f56a2c1e1d01e
zfletch <zfletch2@gmail.com>
2019-03-21 10:37:13 -0400

    remove graph code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,53 +1,42 @@
 import React, { Fragment } from 'react';
 
-import { GraphType } from '../../types';
-import { ReactComponent as FCAMockup } from './FCA-mockup.svg'
+import { ReactComponent as FCAMockup } from './FCA-mockup.svg';
 
 import Hero from '../Hero';
-import Graph from '../Graph';
 
-const Home = ({ match: { params: { graph = 'characteristics' } } }) => {
-  // eslint-disable-next-line no-undef
-  const { userAgent } = navigator;
-
-  return (
-    <Fragment>
-      <Hero
-        title="Birds in Ancient Mythology"
-        text="Exploring Ornithology and Classics through D'Arcy Thompson's Glossary of Greek Birds"
-      />
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 offset-md-2 text-center">
-            <h2>
+const Home = () => (
+  <Fragment>
+    <Hero
+      title="Birds in Ancient Mythology"
+      text="Exploring Ornithology and Classics through D'Arcy Thompson's Glossary of Greek Birds"
+    />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 offset-md-2 text-center">
+          <h2>
               Browse Ancient Bird Myths by Concept
-            </h2>
-            <p>
+          </h2>
+          <p>
               Type search terms to populate the graph with stories!
-            </p>
-          </div>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <input className="form-control form-control-lg" placeholder="aithyia" disabled />
         </div>
       </div>
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 offset-md-2">
-            <input className="form-control form-control-lg" placeholder="aithyia" disabled />
-          </div>
-        </div>
-
-        <div className="row pt-3">
-          <div className="col-12">
-            <FCAMockup/>
-          </div>
+      <div className="row pt-3">
+        <div className="col-12">
+          <FCAMockup />
         </div>
       </div>
-    </Fragment>
-  );
-};
-
-Home.propTypes = {
-  match: GraphType.isRequired,
-};
+    </div>
+  </Fragment>
+);
 
 export default Home;
--- src/types/index.js
+++ src/types/index.js
@@ -1,11 +1,9 @@
 import EditionType from './EditionType';
-import GraphType from './GraphType';
 import HistoryType from './HistoryType';
 import MatchType from './MatchType';
 
 export {
   EditionType,
-  GraphType,
   HistoryType,
   MatchType,
 };
