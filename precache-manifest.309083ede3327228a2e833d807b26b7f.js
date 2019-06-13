6f565208e6ceb32d64d8346454279117c8e85d6e
zfletch <zfletch2@gmail.com>
2019-06-13 13:41:16 +0100

    Updates

---
+++ precache-manifest.309083ede3327228a2e833d807b26b7f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1a5fb8b90eda4f469b1340601176d0",
+    "url": "/darcy/index.html"
+  },
+  {
+    "revision": "a0b9196744449b3eecf2",
+    "url": "/darcy/static/css/2.306cb585.chunk.css"
+  },
+  {
+    "revision": "e133a2073aa409be235b",
+    "url": "/darcy/static/css/main.164603b8.chunk.css"
+  },
+  {
+    "revision": "a0b9196744449b3eecf2",
+    "url": "/darcy/static/js/2.e45ec9e6.chunk.js"
+  },
+  {
+    "revision": "e133a2073aa409be235b",
+    "url": "/darcy/static/js/main.5d077d8b.chunk.js"
+  },
+  {
+    "revision": "e9a5366f9106899c8fc2",
+    "url": "/darcy/static/js/runtime~main.b5ede6ac.js"
+  },
+  {
+    "revision": "fdaf2524f92326b33f529a95a3cd43d9",
+    "url": "/darcy/static/media/FCA-mockup.fdaf2524.svg"
+  },
+  {
+    "revision": "84a4a3ef4252fd440c6f76a4933bfb63",
+    "url": "/darcy/static/media/aithyia-1.84a4a3ef.jpg"
+  },
+  {
+    "revision": "2424b19c6190d89aac93761e3fbb55c9",
+    "url": "/darcy/static/media/aithyia-2.2424b19c.jpg"
+  },
+  {
+    "revision": "7da9ddf753feceae3ac4c2bdf02a106d",
+    "url": "/darcy/static/media/aithyia.7da9ddf7.svg"
+  },
+  {
+    "revision": "01337cbf8cb58f21e1411c4c9ae23c87",
+    "url": "/darcy/static/media/arimo-latin-400.01337cbf.woff2"
+  },
+  {
+    "revision": "6d15a1bb418e9f34d0606fd2d568b429",
+    "url": "/darcy/static/media/arimo-latin-400.6d15a1bb.woff"
+  },
+  {
+    "revision": "422dc78ce039a6a6496e2cbb0ed62eb4",
+    "url": "/darcy/static/media/arimo-latin-400italic.422dc78c.woff"
+  },
+  {
+    "revision": "cc7569b29c944be9246be8c439ff9da4",
+    "url": "/darcy/static/media/arimo-latin-400italic.cc7569b2.woff2"
+  },
+  {
+    "revision": "1383e57eb85ca99948e7ed69efbc1a76",
+    "url": "/darcy/static/media/arimo-latin-700.1383e57e.woff2"
+  },
+  {
+    "revision": "e97a2f2baf901ad2d2c3d7649bffdd22",
+    "url": "/darcy/static/media/arimo-latin-700.e97a2f2b.woff"
+  },
+  {
+    "revision": "2e7c6c6f35ee42fbd1264b169410c9bb",
+    "url": "/darcy/static/media/arimo-latin-700italic.2e7c6c6f.woff2"
+  },
+  {
+    "revision": "505d2fd4a9acc32cba0c56d7a96e6ed1",
+    "url": "/darcy/static/media/arimo-latin-700italic.505d2fd4.woff"
+  },
+  {
+    "revision": "bb83941b3511d850ca9bea826c35f65a",
+    "url": "/darcy/static/media/birds.bb83941b.jpg"
+  },
+  {
+    "revision": "7d8e9ca56cc83ee25d264edbe224472a",
+    "url": "/darcy/static/media/glaux-1.7d8e9ca5.png"
+  },
+  {
+    "revision": "b3d34c730968aa9f98afec6c48118360",
+    "url": "/darcy/static/media/glaux.b3d34c73.svg"
+  }
+]);
