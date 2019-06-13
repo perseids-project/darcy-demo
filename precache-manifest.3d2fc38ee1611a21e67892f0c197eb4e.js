4010f45fcecd69e065f04a2ee8042ef1edc42aaf
zfletch <zfletch2@gmail.com>
2019-06-13 13:40:46 +0100

    Updates

---
+++ precache-manifest.3d2fc38ee1611a21e67892f0c197eb4e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76d71ff691c66de5f85e1ed01eea454",
+    "url": "/darcy-demo/index.html"
+  },
+  {
+    "revision": "b219196fb070c964db3b",
+    "url": "/darcy-demo/static/css/2.535e0524.chunk.css"
+  },
+  {
+    "revision": "59ea9610acbe6a8acd3f",
+    "url": "/darcy-demo/static/css/main.9318cdbd.chunk.css"
+  },
+  {
+    "revision": "b219196fb070c964db3b",
+    "url": "/darcy-demo/static/js/2.e45ec9e6.chunk.js"
+  },
+  {
+    "revision": "59ea9610acbe6a8acd3f",
+    "url": "/darcy-demo/static/js/main.030f5869.chunk.js"
+  },
+  {
+    "revision": "7918b6b39ac504e2dd13",
+    "url": "/darcy-demo/static/js/runtime~main.7b0c762a.js"
+  },
+  {
+    "revision": "fdaf2524f92326b33f529a95a3cd43d9",
+    "url": "/darcy-demo/static/media/FCA-mockup.fdaf2524.svg"
+  },
+  {
+    "revision": "84a4a3ef4252fd440c6f76a4933bfb63",
+    "url": "/darcy-demo/static/media/aithyia-1.84a4a3ef.jpg"
+  },
+  {
+    "revision": "2424b19c6190d89aac93761e3fbb55c9",
+    "url": "/darcy-demo/static/media/aithyia-2.2424b19c.jpg"
+  },
+  {
+    "revision": "7da9ddf753feceae3ac4c2bdf02a106d",
+    "url": "/darcy-demo/static/media/aithyia.7da9ddf7.svg"
+  },
+  {
+    "revision": "01337cbf8cb58f21e1411c4c9ae23c87",
+    "url": "/darcy-demo/static/media/arimo-latin-400.01337cbf.woff2"
+  },
+  {
+    "revision": "6d15a1bb418e9f34d0606fd2d568b429",
+    "url": "/darcy-demo/static/media/arimo-latin-400.6d15a1bb.woff"
+  },
+  {
+    "revision": "422dc78ce039a6a6496e2cbb0ed62eb4",
+    "url": "/darcy-demo/static/media/arimo-latin-400italic.422dc78c.woff"
+  },
+  {
+    "revision": "cc7569b29c944be9246be8c439ff9da4",
+    "url": "/darcy-demo/static/media/arimo-latin-400italic.cc7569b2.woff2"
+  },
+  {
+    "revision": "1383e57eb85ca99948e7ed69efbc1a76",
+    "url": "/darcy-demo/static/media/arimo-latin-700.1383e57e.woff2"
+  },
+  {
+    "revision": "e97a2f2baf901ad2d2c3d7649bffdd22",
+    "url": "/darcy-demo/static/media/arimo-latin-700.e97a2f2b.woff"
+  },
+  {
+    "revision": "2e7c6c6f35ee42fbd1264b169410c9bb",
+    "url": "/darcy-demo/static/media/arimo-latin-700italic.2e7c6c6f.woff2"
+  },
+  {
+    "revision": "505d2fd4a9acc32cba0c56d7a96e6ed1",
+    "url": "/darcy-demo/static/media/arimo-latin-700italic.505d2fd4.woff"
+  },
+  {
+    "revision": "bb83941b3511d850ca9bea826c35f65a",
+    "url": "/darcy-demo/static/media/birds.bb83941b.jpg"
+  },
+  {
+    "revision": "7d8e9ca56cc83ee25d264edbe224472a",
+    "url": "/darcy-demo/static/media/glaux-1.7d8e9ca5.png"
+  },
+  {
+    "revision": "b3d34c730968aa9f98afec6c48118360",
+    "url": "/darcy-demo/static/media/glaux.b3d34c73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/darcy-demo/precache-manifest.9e9df91ba45ad75def41a1808ff8a66d.js"
+  "/darcy-demo/precache-manifest.3d2fc38ee1611a21e67892f0c197eb4e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/darcy-demo/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/darcy-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
